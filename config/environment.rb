690c883c473da29d44723fd268c1944a7a6481a9
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-03 15:04:59 -0700

    Configuration setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Transform keys in responses for use with JavaScript
+Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true

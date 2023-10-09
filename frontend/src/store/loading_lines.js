f9c2d7d2e9caa3762232860330ac4d568cd7e03b
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-09 13:12:55 -0700

    Add icons and fonts

---
+++ frontend/src/store/loading_lines.js
@@ -0,0 +1,16 @@
+export const LOADING_LINES = {
+  LOADING_LINE_1: "Discord was almost called Bonfire before we picked our name. It was meant to be nice and cozy.",
+  LOADING_LINE_2: "Discord was almost called Wyvern before we picked our name. Not too proud of that one.",
+  LOADING_LINE_3: "Our logo's name is Clyde.",
+  LOADING_LINE_4: 'There are a bunch of hidden "Easter Eggs" in the app that happen when you click certain things...',
+  LOADING_LINE_5: "Discord started as a game company making a mobile game called Fates Forever.",
+  LOADING_LINE_6: "Discord’s official birthday is May 13, 2015.",
+  LOADING_LINE_7: "We came up with the idea of Discord Nitro over morning breakfast potatoes.",
+  LOADING_LINE_8: "Our mascot, Wumpus, was originally created as a character with no friends :(",
+  LOADING_LINE_9: "In Discord's early days, light theme was the only theme. Scary times.",
+  LOADING_LINE_11: "Our HypeSquad program has three houses you can be sorted in to by taking the in-app quiz: Bravery, Balance, and Brilliance.",
+  LOADING_LINE_12: "The character on our 404 page is a robot hamster named Nelly.",
+  LOADING_LINE_13: "You can play our version of the Snake game on our 404 page by pressing a secret button.",
+  LOADING_LINE_14: "There's a very small—and we mean small—chance you can get a secret ringtone when calling someone. Good luck!",
+  LOADING_LINE_15: "Our old Partner mascot was an elf named Springle. He recently retired."
+}

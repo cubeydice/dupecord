d022c719364e79b1d821f32b43ebf3688239e9d4
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-09 19:50:34 -0700

    Format splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginForm from './components/LoginForm';
 import RegisterForm from "./components/RegisterForm";
+import SplashPage from "./components/SplashPage";
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/"><SplashPage /></Route>
       <Route path="/login"><LoginForm /></Route>
       <Route path="/register"><RegisterForm /></Route>
     </Switch>
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,7 +3,49 @@ import './SplashPage.css'
 const SplashPage = () => {
 
   return (
-    <div className='banner'></div>
+    <>
+    <main>
+      <h1>IMAGINE A PLACE...</h1>
+        <p>...where you can belong to a school club, a gaming group, or a worldwide art community.
+        Where just you and a handful of friends can spend time together.
+        A place that makes it easy to talk every day and hang out more often.
+        </p>
+    </main>
+
+    <section>
+      <h1>Create an invite-only place where you belong</h1>
+        <figure><svg xmlns='../../../assets/splash/splash_server.svg' alt='server_img'/></figure>
+        <p>Dupecord servers are organized into topic-based channels where you can collaborate,
+        share, and just talk about your day without clogging up a group chat.
+        </p>
+    </section>
+
+    <section>
+      <h1>Where hanging out is easy</h1>
+        <p>Grab a seat in a voice channel when you’re free.
+        Friends in your server can see you’re around and instantly pop in to talk without having to call.
+        </p>
+        <figure><embed src='../../../assets/splash/splash_voice.svg' alt='voice_img'/></figure>
+    </section>
+
+    <section>
+      <h1>From few to a fandom</h1>
+        <p>Get any community running with moderation tools and custom member access.
+        Give members special powers, set up private channels, and more.
+        </p>
+    </section>
+
+    <section>
+      <h1>RELIABLE TECH FOR STAYING CLOSE</h1>
+        <p>Low-latency voice and video feels like you’re in the same room.
+        Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.
+        </p>
+    </section>
+
+    <section>
+      <h1>Ready to start your journey?</h1>
+    </section>
+    </>
   )
 }
 

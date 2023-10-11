d4bfe1d642ad9bbaabc5f00313bb828e02576d81
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-10 21:42:38 -0700

    Add icons

---
+++ frontend/src/components/ProfileBar/index.js
@@ -0,0 +1,12 @@
+import { useDispatch, useSelector } from "react-redux";
+
+const ProfileBar = () => {
+  const dispatch = useDispatch;
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default ProfileBar;

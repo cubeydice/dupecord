6414e047b23768e74d690d405496e63d07a67975
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-19 10:13:38 -0700

    Add conditional to intro message in channel if topic exists

--- frontend/src/components/Messages/index.js
+++ frontend/src/components/Messages/index.js
@@ -5,13 +5,20 @@ import { useParams } from "react-router-dom/cjs/react-router-dom";
 const Messages = ({channels}) => {
   const { serverId, channelId } = useParams();
   const channel = channels[channelId] || {}
+  const introMessage = () => {
+    if (channel.topic) {
+      return channel.topic;
+    } else {
+      return `This is the start of the #${channel.name} channel.`;
+    };
+  };
 
   return (
     <>
       <div className="messages">
         <HeaderBar serverId = {serverId} channel={channel}/>
         <h1>Welcome to #{channel.name}!</h1>
-        <p>This is the start of the #{channel.name} channel.</p>
+        <p>{introMessage()}</p>
       </div>
     </>
   )

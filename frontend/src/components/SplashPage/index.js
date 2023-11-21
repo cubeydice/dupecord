7f6ce672c84308686d423a6083bfa5edb836b9ad
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-11-21 15:38:43 -0800

    Update splash

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -25,24 +25,21 @@ const SplashPage = () => {
     <div className='splash-body'>
       <nav className='splash-nav'>
         <Link to={'/'} className='dupecord'>dupecord</Link>
-      { (!sessionUser) ?
-      <Link to={'/login'}><button className='nav-login-button'>Login</button></Link>
-      : <button className='nav-login-button' onClick={handleLogout}>Logout</button>}
+        { (!sessionUser) ?
+        <Link to={'/login'}><button className='nav-login-button'>Login</button></Link>
+        : <button className='nav-login-button' onClick={handleLogout}>Logout</button>}
       </nav>
-      <div className='banner'>
-        <CloudsSvg className='banner1'/>
-        <Hero1Svg className='banner2'/>
-        <Hero2Svg className='banner3'/>
-      </div>
+
       <main className='splash-main'>
+
         <div>
-        <h1 className='title'>IMAGINE A PLACE...</h1>
-          <p>...where you can belong to a school club, a gaming group, or a worldwide art community.
+          <h1 className='title'>IMAGINE A PLACE...</h1>
+          <p id="main-description">...where you can belong to a school club, a gaming group, or a worldwide art community.
           Where just you and a handful of friends can spend time together.
           A place that makes it easy to talk every day and hang out more often.
           </p>
         </div>
-        <div>
+        <div className='splash-main-buttons'>
           <a href='https://github.com/cubeydice/dupecord' className='nav-link' target="_blank" rel="noreferrer">
             <button className='main-buttons'>
               <GithubSvg id='github-icon'/>
@@ -56,11 +53,17 @@ const SplashPage = () => {
             </button>
           </a>
         </div>
+        <div className='banner'>
+        <CloudsSvg id='banner1'/>
+        <Hero1Svg id='banner2'/>
+        <Hero2Svg id='banner3'/>
+      </div>
       </main>
+
 
       <section>
         <ServerSvg/>
-        <div>
+        <div className='description'>
           <h1>Create an invite-only place where you belong</h1>
           <p>Dupecord servers are organized into topic-based channels where you can collaborate,
           share, and just talk about your day without clogging up a group chat.
@@ -69,7 +72,7 @@ const SplashPage = () => {
       </section>
 
       <section>
-        <div>
+        <div className='description'>
           <h1>Where hanging out is easy</h1>
           <p>Grab a seat in a voice channel when you’re free.
           Friends in your server can see you’re around and instantly pop in to talk without having to call.
@@ -80,7 +83,7 @@ const SplashPage = () => {
 
       <section>
         <ModerationSvg/>
-        <div>
+        <div className='description'>
           <h1>From few to a fandom</h1>
           <p>Get any community running with moderation tools and custom member access.
           Give members special powers, set up private channels, and more.
@@ -91,42 +94,48 @@ const SplashPage = () => {
       <section className='splash-video'>
 
           <h1>RELIABLE TECH FOR STAYING CLOSE</h1>
+
           <p>Low-latency voice and video feels like you’re in the same room.
             Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.
           </p>
-          <p className='splash-video-svg-containter'>
+
           <img
-                src={require("./assets/splash_video.svg").default}
-                alt="splash-videog"
-            />
-          </p>
+            src={require("./assets/splash_video.svg").default}
+            alt="splash-videog"
+          />
+
           <p className='splash-journey'>
             <SparklesSvg/>
             <span>Ready to start your journey?</span>
+
             <Link to={'/login'}>
-            <button className='main-buttons'>
-              Login
-            </button>
+              <button className='main-buttons'>
+                Login
+              </button>
             </Link>
           </p>
       </section>
 
       <footer>
         <div id="footer">
-          <div className='footer-links'>
-            <h2>Let's Connect!</h2>
-            <li>
-              <a href='https://github.com/cubeydice/dupecord' className='nav-link' target="_blank" rel="noreferrer">
-              <img src={require ('./assets/logos/github-mark-white.png')} alt='GitHub'/>
-              &ensp;Github
-              </a>
-            </li>
-            <li>
-              <a href='https://www.linkedin.com/in/queen-belle-d-118b7859/'className='nav-link'  target="_blank" rel="noreferrer">
-                <img src={require ('./assets/logos/LI-mark.png')} alt='linkedIn'/>
-                &ensp;LinkedIn
-              </a>
-            </li>
+          <div id="footer-top">
+            <div className='footer-links'>
+              <h2>Let's Connect!</h2>
+              <ul>
+                <li>
+                  <a href='https://github.com/cubeydice/dupecord' className='nav-link' target="_blank" rel="noreferrer">
+                  <img src={require ('./assets/logos/github-mark-white.png')} alt='GitHub'/>
+                  &ensp;Github
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.linkedin.com/in/queen-belle-d-118b7859/'className='nav-link'  target="_blank" rel="noreferrer">
+                    <img src={require ('./assets/logos/LI-mark.png')} alt='linkedIn'/>
+                    &ensp;LinkedIn
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <hr/>
           <div id="footer-bottom">
